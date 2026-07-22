d30902b4333bc73131395b41749ea5ad6e44be95
THÁI VĂN THI <thaivanthi2005@gmail.com>
2026-07-22 08:36:09 +0700

    setup CORS

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
 import mainV1Router from "./api/v1/routes/index.route";
-
+import cors from "cors";
 const app: Express = express();
 
 const port: number | string = process.env.PORT || 3000;
 app.use(express.json());
-
+app.use(cors());
 dotenv.config();
 database.connect();
 
